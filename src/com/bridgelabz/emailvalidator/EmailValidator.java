5bbdd82bb81c9ad7c19c4404cc7539b89e3b24ed
govind6498 <govindsingh6498@gmail.com>
2021-09-16 16:40:00 +0530

    REFECTOR:Method to ensure email begins with Alphabate

--- src/com/bridgelabz/emailvalidator/EmailValidator.java
+++ src/com/bridgelabz/emailvalidator/EmailValidator.java
@@ -1,7 +1,27 @@
 package com.bridgelabz.emailvalidator;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class EmailValidator {
+	public static void checkValidEmail(String email) {
+		boolean isEmail;
+		String emailRegex="^[a-zA-Z][.@a-zA-Z]*$";
+		Pattern patternObject = Pattern.compile(emailRegex);
+		if(email==null) {
+			isEmail = false;
+		}
+		Matcher matcherObject = patternObject.matcher(email);
+		isEmail = matcherObject.matches();
+		if(isEmail) {
+			System.out.println(email+" is a Valid Email Address");
+			System.exit(0);
+		}
+		System.out.println(email+" is not a Valid Email Address");
+	}
 	public static void main(String[] args) {
-		System.out.println("Welcome to Email Validator");
+		String email = "abc.xyz@bridgelabz.co.in";
+		checkValidEmail(email);
+
+		email = ".abc@abc.com";
+		checkValidEmail(email);
 	}
 }
